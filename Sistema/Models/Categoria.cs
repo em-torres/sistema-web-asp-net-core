bbbd7d85997a89ba5399496e26dce4dac6fe4428
Edwin Marte Torres <kdgpro15@gmail.com>
2019-05-21 12:25:59 -0400

    Starting project

---
+++ Sistema/Models/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using DispStr = Sistema.UserStrings.DisplayStrings;
+using ErStr = Sistema.UserStrings.ErrorStrings;
+using Sistema.UserStrings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        [Required(ErrorMessage = ErStr.ModelReqError)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = ErStr.CatNombreError)]
+        public string Nombre { get; set; }
+
+        [StringLength(256, ErrorMessage = ErStr.CatDescripcionError)]
+        [Display(Name = DispStr.GenDescDisplay)]
+        public string Descripcion { get; set; }
+
+        public Boolean Estado { get; set; }
+    }
+}
---
+++ Sistema/UserStrings/ErrorStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.UserStrings
+{
+    public class ErrorStrings
+    {
+        // Errores Generales
+        public const string ModelReqError = "Campo Requerido.";
+        public const string Excess50Error = "Debe de tener de 3 a 50 caracteres.";
+        public const string Excess256Error = "No debe exceder los 256 caracteres.";
+
+        // Errores en Modelo Categoria
+        public const string CatNombreError = "El nombre debe de tener de 3 a 50 caracteres.";
+        public const string CatDescripcionError = "La descripción no debe exceder los 256 caracteres.";
+    }
+}
