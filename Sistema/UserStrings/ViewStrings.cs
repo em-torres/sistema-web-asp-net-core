9f867d8f176a73f6f9c932a3d9d720122b742735
Edwin Marte Torres <kdgpro15@gmail.com>
2019-05-22 11:37:42 -0400

    Modifying CategoriaModel Views and creating ViewStrings File

---
+++ Sistema/UserStrings/ViewStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.UserStrings
+{
+    public class ViewStrings
+    {
+        // Strings Generales
+        public const string NewEsp = "Nuevo";
+        public const string CreateEsp = "Crear";
+        public const string EditEsp = "Editar";
+        public const string DeleteEsp = "Eliminar";
+        public const string DetailEsp = "Detalles";
+        public const string BackIndexEsp = "Regresar al Listado";
+        public const string IndexEsp = "Indice";
+        public const string DeleteConfEsp = "¿Estás seguro de eliminar este registro?";
+        public const string SaveEsp = "Guardar";
+        public const string CopyrightEsp = "2019 - Sistema Académico";
+
+        // Strings Modelos
+        public const string ModCatNombre = "Categoría";
+    }
+}
